c7fab36060ec90bf6d39cd5325c9172cce8b9efe
w2ca <willybueno090@gmail.com>
2025-01-13 10:24:14 -0300

    Modifica Stubs de Models e Migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,19 +7,18 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'id' => '01jhfwh6y3wst321j8rxx6nvzb',
+            'name' => 'Jhon Doe',
+            'email' => 'admin@admin.com',
+            'password' => '$2y$12$1e.nycxA9BwobQ8LdOppZOGelA4HEtZ1QyhKPLEUu6y1gmShfOmHm',
+            'remember_token' => 'ZJ9dbMSmhYO1HrDwu0eQd46VVhdN62EoXSoYDFQQCNX1ZsiysYJXs8Wbh74S',
         ]);
     }
 }
