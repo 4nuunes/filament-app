c360c5cdb73cef4aac11ffb383999ba65fa99bda
alessandronuunes <alessandronuunes@gmail.com>
2025-01-15 14:46:54 -0300

    feat: adicionado o resource de usuario

--- database/migrations/2025_01_15_171022_add_avatar_url_column_to_users_table.php
+++ database/migrations/2025_01_15_171022_add_avatar_url_column_to_users_table.php
@@ -11,7 +11,7 @@ return new class() extends Migration {
     {
         Schema::table('users', function (Blueprint $table) {
             $table->string('avatar_url')->nullable();
-            dd('oi');
+
         });
     }
 
